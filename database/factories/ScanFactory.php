9d8bed64eac4fa36f82533c242fc6b0e63ade141
DFM-RENDERING <ehsan.sahebzamani@gmail.com>
2020-09-22 07:44:01 -0700

    vahid

---
+++ database/factories/ScanFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Scan;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ScanFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Scan::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
