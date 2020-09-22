fe7874b5addbcc72c65251e1cf1ae523c89fbb44
DFM-RENDERING <ehsan.sahebzamani@gmail.com>
2020-09-22 07:06:07 -0700

    change api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/scans', function (Request $request) {
+    return json_encode(['test' => 'Test']);
+});
